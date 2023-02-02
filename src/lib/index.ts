b9785395d4af07ea62b2238daded09974850f8cd
Muhammed Ali CAN <m.ali.can@macellan.net>
2023-02-02 23:19:16 +0300

    feat(lib): reconfigure package exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,12 @@
-export { Slice } from "./slice/slice";
+export { Slice, createSlice, ReadonlySlice } from "./slice/slice";
+export { Transactor } from "./transactor/index";
+export { StorageTransactor } from "./transactor/storage/storage";
+export { createRecordSlice, RecordSlice } from "./dmi/record";
+export { createVectorSlice, VectorSlice } from "./dmi/vector";
+
+export type {
+  CreateSliceOptions,
+  TypeofSlice,
+  SliceSetter,
+  SliceUpdate,
+} from "./slice/slice";
